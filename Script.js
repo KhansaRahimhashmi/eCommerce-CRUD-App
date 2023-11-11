86a46406eac07299e2317918354908784b544a59
Khansa Rahim <khansarahimm@gmail.com>
2023-11-11 01:01:57 -0800

    First Commit

--- Script.js
+++ Script.js
@@ -1,21 +1,49 @@
-async function deleteProduct(productId) {
+async function fetchAndDisplayPosts() {
     try {
-        const response = await fetch(`https://fakestoreapi.com/products/${productId}`, {
-            method: 'DELETE',
-        });
-
-        console.log('Delete Response:', response);
-
-        if (response.ok) {
-            // Successful delete (HTTP status code 204)
-            console.log('Product deleted successfully');
-            // Reload products after deletion
-            productsContainer.innerHTML = '';
-            await fetchProducts('https://fakestoreapi.com/products');
-        } else {
-            console.error('Failed to delete product. HTTP status:', response.status);
-        }
+        let data = await fetch('https://jsonplaceholder.typicode.com/posts');
+        let response = await data.json();
+        response.forEach(post => displayPost(post));
     } catch (err) {
-        console.error('Delete Error:', err);
+        console.log(err);
     }
 }
+function displayPost(post) {
+    let postsContainer = document.querySelector('.posts');
+    let postElement = document.createElement('div');
+    postElement.classList.add('post');
+    postElement.innerHTML = `
+        <h2 class="post-title">${post.title}</h2>
+        <p class="post-body">${post.body}</p>
+        <div class="post-buttons">
+            <button data-postId="${post.id}" class="delete-post">Delete</button>
+            <button data-postId="${post.id}" class="update-post">Update</button>
+        </div>
+    `;
+    postsContainer.appendChild(postElement);
+}
+async function deletePost(postId) {
+    try {
+        await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`, {
+            method: 'DELETE',
+        });
+        let postElement = document.querySelector(`[data-postId="${postId}"]`);
+        if (postElement) {
+            postElement.remove();
+            console.log(`Post with ID ${postId} deleted successfully.`);
+        } else {
+            console.log(`Post element with ID ${postId} not found in the UI.`);
+        }
+    } catch (err) {
+        console.error(`Error deleting post with ID ${postId}:`, err);
+    }
+}
+
+
+document.querySelector('.posts').addEventListener('click', function (event) {
+        if (event.target.classList.contains('delete-post')) {
+        const postIdToDelete = event.target.dataset.postid;
+        deletePost(postIdToDelete);
+    }
+});
+
+fetchAndDisplayPosts();
